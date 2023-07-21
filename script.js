e6af6e0eaa2f9079dda84f2ddfd73df702a094a4
Riyad <Riyad9876@gmail.com>
2023-07-21 09:59:14 -0400

    working on is-locked class and entering input if empty

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const word = "train"
 
+const gameIsOn = true
 
 const guess1 = ''
 const guess2 = ''
@@ -14,30 +15,57 @@ const guess3Div = document.getElementById('third-row');
 const guess4Div = document.getElementById('fourth-row');
 const guess5Div = document.getElementById('fifth-row');
 
+const gameBody = document.querySelector('.game-body')
 
 // console.log(guess1=== true);
 
 
 body = document.querySelector('body')
-body.addEventListener("keydown", hi)
-
-function hi(e){
-    console.log('hi');
-    console.log(e['key']);
-    console.log(e);
-    const keyPressed = e['key']
-    const newh1 = document.createElement('h1')
-    body.appendChild(newh1)
-    newh1.innerText = keyPressed
-}
-
-
-
-
+// body.addEventListener("keydown", hi)
+// function hi(e){
+//     console.log('hi');
+//     console.log(e['key']);
+//     console.log(e);
+//     const keyPressed = e['key']
+//     const newh1 = document.createElement('h1')
+//     body.appendChild(newh1)
+//     newh1.innerText = keyPressed
+// }
 
 //once return is pressed row is locked in
 
+// console.log(gameBody.children);
 
+
+
+// for (row of gameBody.children){
+//     row.classList.add("is-locked");
+// }
+
+// console.log(gameBody.children);
+
+
+body.addEventListener("keydown", addLetter)
+
+function addLetter(e){
+    for (row of gameBody.children){
+        // console.log(row);
+        if (row.classList.contains('is-locked')){//returns true if row has class is-locked
+            continue //if row is locked skip to next row
+        }
+        else{
+            for (square of row.children){
+                if (square.innerText == ''){
+                    square.innerText = e['key']
+                }
+                else{//if square is not empty
+                    break
+                }
+            }
+            break //only do it to current row
+        } 
+    }
+}
 
 
 
